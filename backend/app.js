c383ca7fb4b994aa20a0adfc06a67bbd9f34a12c
T. Iosifidou <antoiosif87@gmail.com>
2026-04-20 09:22:07 +0300

    feat: add 'register user' functionality

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const auth = require('./routes/auth.routes');
 const user = require('./routes/user.routes');
 const { errorHandler } = require('./middlewares/error-handler.middleware');
 
 app.use(express.json());
+app.use('/api/auth', auth);
 app.use('/api/users', user);
 app.use(errorHandler);
 
